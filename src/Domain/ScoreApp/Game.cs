312bdaf3a9ba51a575a23be39240674a6a1da9f3
De Bock Benny <bdebock@protonmail.com>
2022-02-28 20:59:35 +0100

    Added match and game class

---
+++ src/Domain/ScoreApp/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.ScoreApp
+{
+    public class Game
+    {
+        private List<Match> matches;
+
+        public Game()
+        {
+
+        }
+    }
+}
---
+++ src/Domain/ScoreApp/Match.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Domain.ScoreApp
+{
+    public class Match
+    {
+        private List<Set> sets;
+
+        public Match()
+        {
+
+        }
+    }
+}
